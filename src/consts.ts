853de42b41616158fb079bafc96334a0b981e305
Tom MacWright <tom@macwright.com>
2023-12-06 12:39:02 -0500

    .

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Val Town Blog";
+export const SITE_DESCRIPTION = "Updates and articles from the Val Town team";
