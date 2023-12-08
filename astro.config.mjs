dbfd17cf2133ad42e294afb1e0c5b709647ca69b
Tom MacWright <tom@macwright.com>
2023-12-08 10:13:11 -0500

    Fix sitemap URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://blog.val.town",
   integrations: [astroExpressiveCode(), mdx(), sitemap()],
 });
